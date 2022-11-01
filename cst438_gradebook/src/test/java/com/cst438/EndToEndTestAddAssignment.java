f0cfd6ae3495929e2721b7b9024df388d3a97830
NAPF1 <92123409+NAPF1@users.noreply.github.com>
2022-11-01 16:31:58 -0700

    Added end to end test for adding an assignment

---
+++ cst438_gradebook/src/test/java/com/cst438/EndToEndTestAddAssignment.java
@@ -0,0 +1,147 @@
+package com.cst438;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import com.cst438.domain.Assignment;
+import com.cst438.domain.AssignmentGrade;
+import com.cst438.domain.AssignmentGradeRepository;
+import com.cst438.domain.AssignmentRepository;
+import com.cst438.domain.Course;
+import com.cst438.domain.CourseRepository;
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentRepository;
+
+/*
+ * This example shows how to use selenium testing using the web driver 
+ * with Chrome browser.
+ * 
+ *  - Buttons, input, and anchor elements are located using XPATH expression.
+ *  - onClick( ) method is used with buttons and anchor tags.
+ *  - Input fields are located and sendKeys( ) method is used to enter test data.
+ *  - Spring Boot JPA is used to initialize, verify and reset the database before
+ *      and after testing.
+ *      
+ *  In SpringBootTest environment, the test program may use Spring repositories to 
+ *  setup the database for the test and to verify the result.
+ */
+
+@SpringBootTest
+public class EndToEndTestAddAssignment {
+
+	public static final String CHROME_DRIVER_FILE_LOCATION = "C:/chromedriver_win32/chromedriver.exe";
+
+	public static final String URL = "http://localhost:3000";
+	public static final String TEST_USER_EMAIL = "test@csumb.edu";
+	public static final String TEST_INSTRUCTOR_EMAIL = "dwisneski@csumb.edu";
+	public static final int SLEEP_DURATION = 1000; // 1 second.
+	public static final String TEST_ASSIGNMENT_NAME = "Test Assignment";
+	public static final String TEST_COURSE_TITLE = "Test Course";
+	public static final int TEST_COURSE_ID = 99999;
+	public static final String TEST_COURSE_DATE = "2021-10-31";
+	public static final String TEST_STUDENT_NAME = "Test";
+
+	@Autowired
+	EnrollmentRepository enrollmentRepository;
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	AssignmentGradeRepository assignnmentGradeRepository;
+
+	@Autowired
+	AssignmentRepository assignmentRepository;
+
+	@Test
+	public void addCourseTest() throws Exception {
+
+//		Database setup:  create course		
+		Course c = new Course();
+		c.setCourse_id(TEST_COURSE_ID);
+		c.setInstructor(TEST_INSTRUCTOR_EMAIL);
+		c.setSemester("Fall");
+		c.setYear(2021);
+		c.setTitle(TEST_COURSE_TITLE);
+
+//	    add a student TEST into course 99999
+		Enrollment e = new Enrollment();
+		e.setCourse(c);
+		e.setStudentEmail(TEST_USER_EMAIL);
+		e.setStudentName(TEST_STUDENT_NAME);
+
+		courseRepository.save(c);
+		e = enrollmentRepository.save(e);
+
+		// set the driver location and start driver
+		//@formatter:off
+		// browser	property name 				Java Driver Class
+		// edge 	webdriver.edge.driver 		EdgeDriver
+		// FireFox 	webdriver.firefox.driver 	FirefoxDriver
+		// IE 		webdriver.ie.driver 		InternetExplorerDriver
+		//@formatter:on
+		
+		/*
+		 * initialize the WebDriver and get the home page. 
+		 */
+
+		System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
+		WebDriver driver = new ChromeDriver();
+		// Puts an Implicit wait for 10 seconds before throwing exception
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get(URL);
+		Thread.sleep(SLEEP_DURATION);
+		
+
+		try {
+			WebElement we;
+			
+			// Find the AddAssignment button and click it
+			we = driver.findElement(By.name("addBtn"));
+			we.click();
+			Thread.sleep(SLEEP_DURATION);
+			
+			// Find each of the text boxes and enter necessary data
+			we = driver.findElement(By.name("assignmentName"));
+			we.sendKeys(TEST_ASSIGNMENT_NAME);
+			
+			we = driver.findElement(By.name("dueDate"));
+			we.sendKeys(TEST_COURSE_DATE);
+			
+			we = driver.findElement(By.name("courseId"));
+			we.sendKeys("99999");
+			
+			// Find submit button and click
+			we = driver.findElement(By.name("submit"));
+			we.click();
+			Thread.sleep(SLEEP_DURATION);
+
+		} catch (Exception ex) {
+			throw ex;
+		} finally {
+
+			// Delete the assignment so test can be repeated
+			List<Assignment> list = assignmentRepository.findNeedGradingByEmail(TEST_INSTRUCTOR_EMAIL);
+			for (Assignment a : list) {
+				if (a.getName() == "Test Assignment") {
+					assignmentRepository.delete(a);
+				}
+			}
+			driver.quit();
+		}
+	}
+}
